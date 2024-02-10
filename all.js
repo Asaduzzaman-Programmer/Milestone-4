9960894fffba494b72879710a0cb713aec980539
asad <asaduzzamanprogrammer@gmail.com>
2024-02-10 22:05:14 +0600

    all

---
+++ all.js
@@ -0,0 +1,230 @@
+// const number = 7;
+// if (number > 9){
+//     console.log("boro ekta sonkha");
+// }
+
+// console.log("fuck you ugly bitch");
+
+// let num = 0;
+
+// while(num < 5){
+//     console.log("fucking", num);
+//     num++;
+// }
+
+// let num = 0;
+// while(num < 101){
+//     console.log(" dsadsadsa", num);
+//     num++;
+// }
+// const friends = [ "mask", "bill", "mark", "waren"]
+// for (const bal of friends){
+//     // console.log(bal)
+// }
+
+// for ( let i = 0; i < friends.length; i++){
+//     console.log(i);
+//     console.log(friends[i])
+// }
+
+// const bal = [15,5458,584,5,4,4,54,4,584,58,5,4]
+
+// for (let i = 0; i < bal.length; i++){
+  
+//     console.log(bal[i])
+// }
+
+
+const chap = [1245,545445,1465,1,544,459];
+
+for (let i = 0; i < chap.length; i++){
+    console.log(chap[i])
+}
+
+// numbers in array 
+const numbers = [52, 63, 54, 85];
+console.log(numbers);
+// boolean in array
+const booleans = [true, false, true, false];
+console.log(booleans);
+// strings in array
+const strings = ['abul', 'kabul', 'mokbul', 'safin']
+console.log(strings);
+// mixed array
+const mixed = ["mokbul", 58, true]
+console.log(mixed)
+// for (let i = 0; i <= 10; i++){
+//     console.log(i);
+//     if ( i >= 5){
+//         break;
+//     }
+// }
+
+// let i = 54;
+
+// while(i > 25 ){
+//     if( i < 50){
+//         break;
+//     }
+//     console.log(i);
+//     i--;
+// }
+console.log(5 == 10);
+
+console.log(10 >= 10);
+
+console.log(15 <=10);
+
+console.log(15 >=10);
+
+console.log(10 == 10.1);
+
+console.log(10 == 10);
+
+var applePrice = 40;
+var peyaraPrice = 250;
+
+console.log( applePrice < peyaraPrice);
+
+console.log(10 == "10");
+console.log(10 === "10");
+// while(i > 25 ){
+//     if( i < 50){
+//         break;
+//     }
+//     console.log(i);
+//     i--;
+// }// for (let i = 0; i < 20; i++){
+//     if(i%2===1){
+//         console.log("bijor", i)
+//     }
+//     else(
+//         console.log("jor", i)
+//     )
+// }
+
+// for (i = 0; i <= 30; i++){
+//     // console.log(i)
+//     if(i % 5 ===0 || i % 3 ===0){
+//         console.log(i)
+//     }
+// }
+// let sum = 0;
+// for (i = 0; i <= 30; i++){
+//     // console.log(i)
+//     if(i % 5 ===0){
+//         // console.log(i)
+//         sum = sum + i;
+//         console.log(sum)
+//     }
+// }
+function evenSizedString(str){
+    const size = str.length;
+    console.log(str, size);
+    if(size % 2 === 0){
+        console.log("even size")
+    }
+    else{
+        console.log("odd size")
+    }
+    
+
+}
+
+evenSizedString("Dhaka")
+evenSizedString("Dinazpur")
+evenSizedString("kumillah")
+evenSizedString("rajsahi")
+evenSizedString("rangpur")
+// let sum = 0;
+
+// for (let i = 5; i <= 10; i++){
+//     sum = sum + i;
+//     console.log(i);
+// }
+
+// console.log(sum);
+
+// for(let main = 200; main <= 250: main++){
+//     console.log(main)
+// }
+
+// let sum = 0;
+
+// for (let i = 200; i <= 250; i++){
+//     sum = sum + i;
+// }
+
+// console.log(sum);
+
+
+// for (let num = 0; num < 10; num++){
+//     console.log(num)
+// }
+
+// for (let num = 1; num < 11; num++){
+//     console.log(num)
+// }
+// function borgo(num){
+//     const oho = num*num;
+//     console.log(oho);
+
+// }
+
+// borgo(8);
+// borgo(7)
+
+
+// function jog(num1, num2){
+//     const total = num1 + num2;
+//     console.log(total);
+// }
+
+// jog(65, 5)
+
+// function dif(num1, num2){
+//     const par = num1 - num2;
+//     console.log(par)
+// }
+
+// const fatherAge = 55;
+// const motherAge = 44;
+
+// dif(fatherAge, motherAge)
+if (10 == 13){
+    console.log('I never ever go korea');
+}
+
+else{
+    console.log( "i will go china")
+}
+
+var biriyaniPrice = 550;
+
+if (biriyaniPrice < 500){
+    console.log("give me 25 packet")
+}
+
+else(
+    console.log("give me 10 packet")
+)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
